59e55ca9551b963ce011b78c0ba5a9a1fab93036
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-07-02 17:07:40 +0100

    the main app.html Template was changed

--- .history/try2/src/app/app.component_20230702170456.ts
+++ .history/try2/src/app/app.component_20230702170456.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Hello world!</h1>`,
     // templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'homes';
+    title = 'try2';
 }
--- .history/try2/src/app/app.component_20230702170505.ts
+++ .history/try2/src/app/app.component_20230702170505.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Hello world!</h1>`,
+    
     // templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'homes';
+    title = 'try2';
 }
